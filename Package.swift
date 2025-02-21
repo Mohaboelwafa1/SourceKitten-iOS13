7cbac5f6311de02ead10acc7086cc6fce5c55826
Mohammed Hassan 🍏 <mohammed.hassan.aboelwafa@sap.com>
2025-02-21 18:24:18 +0100

    update to iOS 13

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "SourceKitten",
-    platforms: [.macOS(.v12)],
+    platforms: [.macOS(.v13)],
     products: [
         .executable(name: "sourcekitten", targets: ["sourcekitten"]),
         .library(name: "SourceKittenFramework", targets: ["SourceKittenFramework"])
